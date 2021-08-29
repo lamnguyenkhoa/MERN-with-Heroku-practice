0420bbe48d5e179f0388609f7ce8b8eb3041851a
NKLam <khoalamvn@gmail.com>
2021-08-29 17:40:12 +0700

    Improved CI

--- test/spec.js
+++ test/spec.js
@@ -2,10 +2,11 @@ var request = require("supertest");
 describe("loading express", function () {
   var server;
   beforeEach(function () {
+    delete require.cache[require.resolve("../server")];
     server = require("../server");
   });
-  afterEach(function () {
-    server.close();
+  afterEach(function (done) {
+    server.close(done);
   });
   it("responds to /", function testSlash(done) {
     request(server).get("/").expect(200, done);
