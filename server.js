b7696ba09ca1940b4561dc2a45f0403368f98e93
NKLam <khoalamvn@gmail.com>
2021-08-29 19:26:06 +0700

    Fix mistake that mocha test route overwrite default route

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
+app.get("/test-ci", function (req, res) {
   res.status(200).send("ok");
 });
 
--- test/spec.js
+++ test/spec.js
@@ -8,8 +8,8 @@ describe("loading express", function () {
   afterEach(function (done) {
     server.close(done);
   });
-  it("responds to /", function testSlash(done) {
-    request(server).get("/").expect(200, done);
+  it("responds to /", function testSlashCI(done) {
+    request(server).get("/test-ci").expect(200, done);
   });
   it("404 everything else", function testPath(done) {
     request(server).get("/foo/bar").expect(404, done);
